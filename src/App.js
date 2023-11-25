e2265cec84e5c7cfaf20c4f18c369d1ea8589a52
salizarana <salizarana@gmail.com>
2023-11-25 15:39:14 +0545

    Fix issue.

--- src/App.js
+++ src/App.js
@@ -17,14 +17,6 @@ const App = () => {
 
   const [darkMode, setDarkMode] = useState(false);
 
-  // useEffect(() => {
-  //   const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
-
-  //   if (savedNotes) {
-  //     setNotes(savedNotes);
-  //   }
-  // }, []);
-
   useEffect(() => {
     localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
   }, [notes]);
